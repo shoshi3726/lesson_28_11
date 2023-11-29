14ecd4781917ad684f28deb0cb2a7c93704d92b5
shoshi3726 <shoshi3726@gmail.com>
2023-11-29 09:38:45 +0200

    beginning to 29_11

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,3 +1,4 @@
+const { config } = require("../config/secret");
 const jwt = require("jsonwebtoken");
 
 
@@ -11,7 +12,7 @@ exports.auth = async(req,res,next) => {
   try{
     // מנסה לפענח את הטוקן ויכיל את כל המטען/מידע שבתוכו
     // ובמיוחד את האי די
-    let tokenData = jwt.verify(token, "MaorSecret");
+    let tokenData = jwt.verify(token, config.tokenSecret);
     // דואג להעיבר את המאפיין של הטוקן דאטא לפונקציה הבאה בשרשור
     // שאנחנו מזמנים בנקסט ככה שתיהיה חשופה למידע
     // במקרה הזה האיי די שפענחנו מהטוקן
@@ -21,5 +22,28 @@ exports.auth = async(req,res,next) => {
   }
   catch(err){
    return res.status(401).json({msg:"Token not valid or expired 77777777"})
+  }
+}
+exports.authAdmin = (req,res,next) => {
+  let token = req.header("x-api-key");
+  if(!token){
+    return res.status(401).json({msg:"You need to send token to this endpoint url 7777"})
+  }
+  try{
+    let decodeToken = jwt.verify(token,config.tokenSecret);
+    // check if the role in the token of admin
+    if(decodeToken.role != "admin"){
+      return res.status(401).json({msg:"Token invalid or expired, code: 6A"})
+    }
+   
+    // add to req , so the next function will recognize
+    // the tokenData/decodeToken
+    req.tokenData = decodeToken;
+
+    next();
+  }
+  catch(err){
+    console.log(err);
+    return res.status(401).json({msg:"Token invalid or expired, log in again or you hacker!"})
   }
 }
