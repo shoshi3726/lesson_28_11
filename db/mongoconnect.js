2970269acd9fcb6bcdfbcfb23552188909a079d8
shoshi3726 <shoshi3726@gmail.com>
2023-11-29 10:31:28 +0200

    adding index.html

--- db/mongoconnect.js
+++ db/mongoconnect.js
@@ -7,7 +7,9 @@ main().catch(err => console.log(err));
 async function main() {
   // mongoose.set('strictQuery' , false);
   mongoose.set('strictQuery' , false);
-  await mongoose.connect(`mongodb+srv://${config.userDb}:${config.passDb}@cluster0.uvvgws0.mongodb.net/black23`);
+  await mongoose.connect(
+    `mongodb+srv://${config.userDb}:${config.passDb}@cluster0.uvvgws0.mongodb.net/black23`
+    );
   console.log("mongo connect started");
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
